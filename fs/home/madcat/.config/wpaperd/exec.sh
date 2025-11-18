d0cc729197d5de1c174f394ac55eb07651b90da8
madcat <madcatlord@gmx.net>
2025-11-18 17:19:18 +0700

    styled hyprlock a little

--- fs/home/madcat/.config/wpaperd/exec.sh
+++ fs/home/madcat/.config/wpaperd/exec.sh
@@ -3,8 +3,18 @@
 MONITOR=$1	# Name of Monitor
 WALLPAPER=$2	# Path to current wallpaper
 
+
+
+### Update pywal stuffs
 # -q Executes wal in quiet mode, meaning it'll print nothing
 # -n Prevents wal from attempting to set the wallpaper (fails anyways)
 # -i <PATH> defines which image to generate the color scheme from
 wal -q -n -i "$WALLPAPER"
 source "~/.cache/wal/colors.sh"
+
+
+
+### Update active image
+# NOTE: This is mainly interesting for hyprlock, in order to display the current wallpaper as the lockscreen background
+
+cp $WALLPAPER ~/.config/wpaperd/active_wallpaper
